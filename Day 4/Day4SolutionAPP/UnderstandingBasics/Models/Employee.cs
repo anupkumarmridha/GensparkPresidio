60bd249169235eace04e34cac632c392a48ecae3
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-04-12 12:56:12 +0530

    Added: Day4 before launch

---
+++ Day 4/Day4SolutionAPP/UnderstandingBasics/Models/Employee.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingBasics.Models
+{
+    internal class Employee
+    {
+        //internal int Id;
+        //int Id;
+        //public int GetId()
+        //{
+        //    return Id;
+        //}
+
+        //public void SetId(int eId)
+        //{
+        //    Id= eId;
+        //}
+
+        //public int Id {
+        //    get { return Id; }
+        //    set { Id = value; } 
+
+        //}
+        //public int Id
+        //{
+        //    get => Id;
+        //    set => Id = value;
+        //}
+
+        double salary;
+        public int Id { get; private set; }
+
+        public string Name { get; set; }
+        public double Salary
+        {
+            set
+            {
+                salary = value;
+            }
+            get
+            {
+                return (salary - (salary * 10 / 100));
+            }
+        }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+
+        public Employee()
+        {
+            Id = 0;
+            Name=string.Empty;
+            Email = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+            Salary = 0;
+        }
+
+        public Employee(int id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">Employee Id as Int</param>
+        /// <param name="name">Employee name as string</param>
+        /// <param name="salary">Employee salary as double without TDS deduction</param>
+        /// <param name="dateOfBirth">DateOfBirth as DateTime(yaar,month, day)</param>
+        /// <param name="email">Email as String</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Employee(int id, string name, double salary, DateTime dateOfBirth, string email) : this(id)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Salary = salary;
+            DateOfBirth = dateOfBirth;
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+        }
+
+        /// <summary>
+        /// Print Employee Details
+        /// </summary>
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine($"Employee Id \t:\t{Id}");
+            Console.WriteLine($"Employee Name \t:\t{Name}");
+            Console.WriteLine($"Employee Email \t:\t{Email}");
+            Console.WriteLine($"Employee Date Of Birth \t:\t{DateOfBirth}");
+            Console.WriteLine($"Employee Salary \t:\t{Salary}");
+        }
+
+        /// <summary>
+        /// Display the work done in the duration
+        /// </summary>
+        /// <param name="hours">Hours required for Work</param>
+        /// <returns>Message Work done in the Duration</returns>
+        public string Work(int hours)
+        {
+            Console.WriteLine("Work");
+            return $"Done work for {hours} hours";
+        }
+    }
+}
---
+++ Day 4/Day4SolutionAPP/UnderstandingBasics/Program.cs
@@ -0,0 +1,100 @@
+﻿using UnderstandingBasics.Models;
+
+namespace UnderstandingBasics
+{
+    internal class Program
+    {
+
+        Employee CreateEmployeeByTakingDetailsFromConsole(int id)
+        {
+            //Employee employee = new Employee(id);
+            Employee employee = new (id);
+            Console.WriteLine("Please enter your name");
+            employee.Name = Console.ReadLine();
+            Console.WriteLine("Please enter your DOB");
+            employee.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Please enter Salary");
+            double salary;
+            while(!double.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("Invalid entry! please try again");
+            }
+            employee.Salary = salary;
+            Console.WriteLine("Please enter your email");
+            employee.Email = Console.ReadLine();
+            return employee;
+        }
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee();
+            //employee.Id = 1;
+            //employee.Name = "Test";
+            //employee.Salary = 100000;
+            //employee.DateOfBirth = new DateTime(2000, 12, 18);
+            //employee.Email = "anup@gmail.com";
+
+            //Employee employee1 = new Employee(101)
+            //{
+            //    Name = "Mon",
+            //    DateOfBirth = new DateTime(2001, 05, 06),
+            //    Email="mon@gmail.com",
+            //    Salary=20000,
+            //};
+            //Employee employee2 = new Employee(103, "Sweety", 30000, new DateTime(2000, 05, 07), "sweety@gmail.com");
+            //Console.WriteLine(employee1.Salary);
+            //Console.WriteLine(employee.Salary);
+            //employee.Work(12);
+            //employee2.PrintEmployeeDetails();
+
+            
+            //Array's in C#
+
+            //int[] ages = new int[3];
+            //ages[0] = 1;
+            //ages[1] = 2;
+            //ages[2] = 3;
+
+            //for (int i = 0; i < ages.Length; i++)
+            //{
+            //    Console.WriteLine(ages[i]);
+
+            //}
+
+            //int count = ages.Length - 1;
+
+            //while (count>=0)
+            //{
+            //    Console.WriteLine(ages[count]);
+            //    count--;
+            //}
+
+            //int count = ages.Length - 1;
+
+            //do
+            //{
+            //    if (ages[count]>100)
+            //    {
+            //        Console.WriteLine("Age is Greater then 100");
+            //    }
+            //    Console.WriteLine(ages[count]);
+            //    count--;
+            //} while (count >= 0);
+
+            Employee[] employees = new Employee[3];
+
+            //Program program = new Program();
+            Program program = new();
+
+
+            for(int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = program.CreateEmployeeByTakingDetailsFromConsole(100+i);
+            }
+            for(int i = 0; i < employees.Length; i++)
+            {
+                employees[i].PrintEmployeeDetails();
+            }
+
+        }
+    }
+}
