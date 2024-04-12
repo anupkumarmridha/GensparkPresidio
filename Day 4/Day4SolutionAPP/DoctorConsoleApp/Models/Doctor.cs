61509a34e725a993a89086f5023b38e563113e1c
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-04-12 15:43:16 +0530

    Added: Doctor Problem Solution

---
+++ Day 4/Day4SolutionAPP/DoctorConsoleApp/Models/Doctor.cs
@@ -0,0 +1,47 @@
+﻿namespace DoctorConsoleApp.Models
+{
+    internal class Doctor
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ExperienceInYears { get; set; }
+        public string Qualification { get; set; }
+        public string Speciality { get; set; }
+
+        public Doctor(int id) => Id = id;
+
+        /// <summary>
+        /// Constractor to create a Doctor object
+        /// </summary>
+        /// <param name="id">Provide id as int</param>
+        /// <param name="name">Provide Name as string</param>
+        /// <param name="age">Provide Age as int</param>
+        /// <param name="experienceInYears">Provide ExperienceInYears as int</param>
+        /// <param name="qualification">Provide Qualification as string</param>
+        /// <param name="speciality">Provide speciality as string</param>
+        public Doctor(int id, string name, int age, int experienceInYears, string qualification, string speciality):this(id)
+        {
+            Name = name;
+            Age = age;
+            ExperienceInYears = experienceInYears;
+            Qualification = qualification;
+            Speciality = speciality;
+        }
+
+        /// <summary>
+        /// Display the Doctor details
+        /// </summary>
+        public void DisplayDetails()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Doctor ID:\t{Id}");
+            Console.WriteLine($"Name:\t\t{Name}");
+            Console.WriteLine($"Age:\t\t{Age}");
+            Console.WriteLine($"Experience:\t{ExperienceInYears} years");
+            Console.WriteLine($"Qualification:\t{Qualification}");
+            Console.WriteLine($"Speciality:\t{Speciality}");
+        }
+    }
+}
---
+++ Day 4/Day4SolutionAPP/DoctorConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using DoctorConsoleApp.Models;
+
+namespace DoctorConsoleApp
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Read only non numeric string from the console
+        /// </summary>
+        /// <param name="fieldName">Provide fieldName as string</param>
+        /// <returns></returns>
+        string ReadNonNumericStringFromConsole(string fieldName)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine($"Please enter {fieldName}:");
+                input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input) || input.Any(char.IsDigit))
+                {
+                    Console.WriteLine($"{fieldName} cannot be empty or contain numbers. Please try again.");
+                }
+            } while (string.IsNullOrWhiteSpace(input) || input.Any(char.IsDigit));
+
+            return input;
+        }
+
+        /// <summary>
+        /// Read Positive Integer only from the Console
+        /// </summary>
+        /// <param name="fieldName">Provide fieldName as string</param>
+        /// <returns></returns>
+        int ReadPositiveIntegerFromConsole(string fieldName)
+        {
+            int value;
+            Console.WriteLine($"Please enter {fieldName}:");
+            while(!int.TryParse(Console.ReadLine(), out value) || value <= 0)
+            {
+                Console.WriteLine($"Invalid input for {fieldName}! Please enter a positive integer.");
+            }
+            return value;
+        }
+
+
+
+        /// <summary>
+        /// Create Doctor object by taking input from console
+        /// </summary>
+        /// <param name="id">Provide Id as int</param>
+        /// <returns></returns>
+        Doctor CreateDoctorByTakingDetailsFromConsole(int id)
+        {
+            Doctor doctor = new(id);
+            doctor.Name = ReadNonNumericStringFromConsole("Name");
+            doctor.Age = ReadPositiveIntegerFromConsole("Age");
+            doctor.ExperienceInYears = ReadPositiveIntegerFromConsole("Experience in years");
+            doctor.Qualification = ReadNonNumericStringFromConsole("Qualification");
+            doctor.Speciality = ReadNonNumericStringFromConsole("Speciality");
+            return doctor;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program program = new();
+            int n;
+            Console.WriteLine("Please enter number of Doctors Details you want to add: ");
+            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            {
+                Console.WriteLine("Invalid number! Please enter a valid number > 0:");
+            }
+            Doctor[] doctors = new Doctor[n];
+            for (int i = 0; i < n; i++)
+            {
+                doctors[i]=program.CreateDoctorByTakingDetailsFromConsole(100+i);
+            }
+
+            for(int i = 0;i < doctors.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Details of Doctor Id {doctors[i].Id} ");
+                doctors[i].DisplayDetails();
+            }
+
+        }
+    }
+}
