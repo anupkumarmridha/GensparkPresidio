985c620c284e56982b407a09d64393144bc42bb1
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-04-24 13:04:18 +0530

    Added day 11 before launch

---
+++ Day 11/ShoppingSolution/ShoppingDALLibrary/CartItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using ShoppingModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDALLibrary
+{
+    public class CartItemRepository : AbstractRepository<int, CartItem>
+    {
+        public override CartItem Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CartItem GetByKey(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CartItem Update(CartItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day 11/ShoppingSolution/ShoppingDALLibrary/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using ShoppingModelLibrary.Exceptions;
+using ShoppingModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDALLibrary
+{
+    public class ProductRepository : AbstractRepository<int, Product>
+    {
+        public override Product Delete(int key)
+        {
+            Product product = GetByKey(key);
+            if (product != null)
+            {
+                items.Remove(product);
+            }
+            return product;
+        }
+
+        public override Product GetByKey(int key)
+        {
+            //for (int i = 0; i < items.Count; i++)
+            //{
+            //    if (items[i].Id == key)
+            //        return items[i];
+            //}
+            //Predicate<Product> findProduct =(p)=>p.Id==key;
+            //Product product = items.ToList().Find(findProduct);
+
+            //Product product = items.ToList().Find((p) => p.Id == key);
+
+            Product product = items.FirstOrDefault(p => p.Id == key);
+            return product;
+            //throw new NoCustomerWithGiveIdException();
+        }
+
+        public override Product Update(Product item)
+        {
+            Product product = GetByKey(item.Id);
+            if (product != null)
+            {
+                product = item;
+            }
+            return product;
+        }
+    }
+}
