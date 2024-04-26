c77e0ce538201ed377e770f5908ff9711e3a956b
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-04-26 12:20:00 +0530

    added day 13 before launch

--- Day 11/ShoppingSolution/ShoppingFE/Program.cs
+++ Day 11/ShoppingSolution/ShoppingFE/Program.cs
@@ -5,109 +5,51 @@ namespace ShoppingFE
 {
     internal class Program
     {
-        //public delegate int calcDel(int n1, int n2);//creating a type that refferes to a method
-        //void Calculate(calcDel cal)
+        //async Task<int> GetResultFromDatabaseServer()
         //{
-        //    int n1 = 10, n2 = 20;
-        //    int result = cal(n1, n2);
-        //    Console.WriteLine($"The sum of {n1} and {n2} is {result}");
-        //}
-        //public int Add(int num1, int num2)
-        //{
-        //    return (num1 + num2);
-        //}
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Hello, World!");
-        //    Program program = new Program();
-        //    calcDel c1 = new calcDel(program.Add);
-        //    program.Calculate(c1);
-
+        //    Thread.Sleep(10000);
+        //    return new Random().Next();
         //}
 
-        //public delegate int calcDel(int n1, int n2);//creating a type that refferes to a method
-        //public delegate float calcDelFloat(float n1, float n2);//creating a type that refferes to a method
-        //public delegate T calcDel<T>(T n1, T n2);//creating a generic  type that refferes to a method
-        //void Calculate(calcDel<int> cal, int n1, int n2)
-        //{
+        void print() { 
+        
+            for(int i=0;i < 10; i++)
+            {
+                Console.WriteLine("By "+Thread.CurrentThread.Name+" ",i);
+            }
+        }
+        void printThredSafe() {
 
-        //    int result = cal(n1, n2);
-        //    Console.WriteLine($"The sum of {n1} and {n2} is {result}");
-        //}
-        //void Calculate(Func<int, int, int> cal)
-        //{
-        //    int n1 = 10, n2 = 20;
-        //    int result = cal(n1, n2);
-        //    Console.WriteLine($"The sum of {n1} and {n2} is {result}");
-        //}
-        //public int Add(int num1, int num2)
-        //{
-        //    return (num1 + num2);
-        //}
-        static void Main(string[] args)
+            lock (this)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("By " + Thread.CurrentThread.Name + " ", i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Program program = new Program();
-            //calcDel c1 = new calcDel(program.Add);
-            //calcDel<int> c1 = new calcDel<int>(program.Add);//Generic type instantiation
-            //calcDel<int> c1 = delegate (int num1, int num2)//You can do if you are alwayts going to use teh ref to use the method
-            //{
-            //    return (num1 + num2);
-            //};
-            //int n1 = 10, n2 = 20;
-            //calcDel<int> c1 = (int num1, int num2) => (num1 + num2);
-            //program.Calculate(c1, n1, n2);
+            //Thread t1 = new Thread(program.print);
+            Thread t1 = new Thread(program.printThredSafe);
+            t1.Name = "Thread 1";
+            //Thread t2 = new Thread(program.print);
+            Thread t2 = new Thread(program.printThredSafe);
+            t2.Name = "Thread 2";
 
-            //Func<int, int, int> c2=(num1, num2)=> (num1 + num2);
-            //program.Calculate(c2);
-
-            //int[] numbers = { 89, 78, 23, 546, 787, 98, 11, 3 };
-            //select * from numbers where num>80
-            // var another = from n in numbers where n > 80 select n;
-            //var another = numbers.Where(n => n > 80);
-            //foreach (int n in another)
-            //    Console.WriteLine(n);
-            //IRepository<int, Customer> customerRepo = new CustomerRepository();
-            //customerRepo.Add(new Customer { Id = 1, Name = "Ramu", Age = 23 });
-            //customerRepo.Add(new Customer { Id = 2, Name = "Ramu", Age = 31 });
-            //customerRepo.Add(new Customer { Id = 3, Name = "Komu", Age = 27 });
-            //var customers = customerRepo.GetAll().ToList();
-            //customers = customers.OrderBy(cust => cust.Name).ThenBy(cust=>cust.Age).ToList();
-            //customers = customers.OrderByDescending(cust => cust.Name).ThenByDescending(cust=>cust.Age).ToList();
-            //foreach (var item in customers)
-            //{
-            //    Console.WriteLine(item);
-
-            //}
-
-            int[] numbers = { 89, 78, 23, 546, 787, 98, 11, 3 };
-
-            int[] evenNumebrs = numbers.EvenCatch();
-            foreach (int n in evenNumebrs)
-                Console.WriteLine(n);
+            t1.Start();
+            t2.Start();
+            await Console.Out.WriteLineAsync("After the Thread call");
+            //var number = program.GetResultFromDatabaseServer();
+            //Console.WriteLine("This is the random number from server " + number);
+            //Console.WriteLine("This is the random number from main" + new Random().Next());
+            ////var number1 = await program.GetResultFromDatabaseServer();
+            //Console.WriteLine(number1);
         }
     }
 
-    public static class StringMethods
-    {
-        public static string Reverse(this string msg)
-        {
-            char[] chars = msg.ToCharArray();
-            Array.Reverse(chars);
-            return new string(chars);
-        }
-
-    }
-    public static class NumberExtension
-    {
-        public static int[] EvenCatch(this int[] nums)
-        {
-            //List<int> result = new List<int>();
-            //foreach (int num in nums)
-            //    if (num % 2 == 0)
-            //        result.Add(num);
-            //return result.ToArray();
-            return nums.Where(num => num % 2 == 0).ToArray();
-        }
-    }
+    
 }
