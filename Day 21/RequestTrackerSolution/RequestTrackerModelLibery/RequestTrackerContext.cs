be05e5cb2eb2ed9fc9bca0f8c47b66a8da398a4d
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-05-09 13:09:04 +0530

    added day 21 morning session

---
+++ Day 21/RequestTrackerSolution/RequestTrackerModelLibery/RequestTrackerContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerModelLibery
+{
+    public class RequestTrackerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=7TXBBX3\SQLEXPRESS;Integrated Security=true;Initial Catalog=dbEmployeeTrackerCF;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Request> Requests { get;set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Request>()
+               .HasOne(r => r.RaisedByEmployee)
+               .WithMany(e => e.RequestsRaised)
+               .HasForeignKey(r => r.RequestRaisedBy)
+               .OnDelete(DeleteBehavior.Restrict)
+               .IsRequired();
+
+            modelBuilder.Entity<Request>()
+               .HasOne(r => r.RequestClosedByEmployee)
+               .WithMany(e => e.RequestsClosed)
+               .HasForeignKey(r => r.RequestClosedBy)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+    }
+}
