4822e69218afa574df23dc70122864b8906782b2
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-05-16 14:35:27 +0530

    Added swagger config for jwt

--- day 24/EmployeeRequestTrackerSolution/EmployeeRequestTrackerAPI/Program.cs
+++ day 24/EmployeeRequestTrackerSolution/EmployeeRequestTrackerAPI/Program.cs
@@ -7,6 +7,7 @@ using EmployeeRequestTrackerAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace EmployeeRequestTrackerAPI
@@ -23,6 +24,32 @@ namespace EmployeeRequestTrackerAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+            });
 
             //Debug.WriteLine(builder.Configuration["TokenKey:JWT"]);
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
