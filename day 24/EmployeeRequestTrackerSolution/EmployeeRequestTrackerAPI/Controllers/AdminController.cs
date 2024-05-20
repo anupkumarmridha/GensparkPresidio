2fa3ee9faf833fe51c46f3edf4d48c18fd36c527
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-05-20 15:48:14 +0530

    added admin operations on request tracker

--- day 24/EmployeeRequestTrackerSolution/EmployeeRequestTrackerAPI/Controllers/AdminController.cs
+++ day 24/EmployeeRequestTrackerSolution/EmployeeRequestTrackerAPI/Controllers/AdminController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeRequestTrackerAPI.Models.DBModels;
+using EmployeeRequestTrackerAPI.Models.DTOModels;
+using EmployeeRequestTrackerAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace EmployeeRequestTrackerAPI.Controllers
 {
@@ -7,5 +12,99 @@ namespace EmployeeRequestTrackerAPI.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly IAdminRequestService _adminRequestService;
+        private readonly ILogger<AdminController> _logger;
+        public AdminController(IAdminRequestService adminRequestService, ILogger<AdminController> logger)
+        {
+            _adminRequestService = adminRequestService;
+            _logger = logger;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("AddRequestSolution")]
+        [ProducesResponseType(typeof(RequestSolution), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<RequestSolution>> AddRequestSolutionByAdmin(AdminRequestSolutionDTO adminRequestSolutionDTO)
+        {
+            try
+            {
+                var employeeId = User.FindFirstValue(ClaimTypes.Name);
+                if (employeeId == null)
+                {
+                    return BadRequest(new ErrorModel(400, "Invalid User"));
+                }
+                var AdminEmployeeId = Convert.ToInt32(employeeId);
+                var result = await _adminRequestService.AddRequestSolutionByAdmin(adminRequestSolutionDTO.RequestId, adminRequestSolutionDTO.SolutionDescription, AdminEmployeeId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while adding request solution");
+                if (ex.InnerException != null)
+                {
+                    _logger.LogError(ex.InnerException, "Inner exception details");
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorModel(500, ex.Message));
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("GetAllRequestsByStatus")]
+        [ProducesResponseType(typeof(IList<Request>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IList<Request>>> GetAllEmployeesRequestsByStatus(string status)
+        {
+            try
+            {
+                var employeeId = User.FindFirstValue(ClaimTypes.Name);
+                if (employeeId == null)
+                {
+                    return BadRequest(new ErrorModel(400, "Invalid User"));
+                }
+                var AdminEmployeeId = Convert.ToInt32(employeeId);
+                var result = await _adminRequestService.GetAllEmployeesRequestsByStatus(AdminEmployeeId, status);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while fetching requests by status");
+                if (ex.InnerException != null)
+                {
+                    _logger.LogError(ex.InnerException, "Inner exception details");
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorModel(500, ex.Message));
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("CloseRequest")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<bool>> MarkedRequestCloseByAdmin(int requestId)
+        {
+            try
+            {
+                var employeeId = User.FindFirstValue(ClaimTypes.Name);
+                if (employeeId == null)
+                {
+                    return BadRequest(new ErrorModel(400, "Invalid User"));
+                }
+                var AdminEmployeeId = Convert.ToInt32(employeeId);
+                var result = await _adminRequestService.MarkedRequestCloseByAdmin(requestId, AdminEmployeeId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while closing the request");
+                if (ex.InnerException != null)
+                {
+                    _logger.LogError(ex.InnerException, "Inner exception details");
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorModel(500, ex.Message));
+            }
+        }
     }
 }
---
+++ day 24/EmployeeRequestTrackerSolution/EmployeeRequestTrackerAPI/Models/DTOModels/AdminRequestSolutionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeRequestTrackerAPI.Models.DTOModels
+{
+    public class AdminRequestSolutionDTO
+    {
+        public int RequestId { get; set; }
+        public string SolutionDescription { get; set; }
+    }
+}
