1e3fb9b7c84f03544b01744687e1ee208dadad8b
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2024-07-17 13:03:31 +0530

    17th july morning session

---
+++ Day 69 17th july/AzureKeyVaultSolution/AzureKeyVault/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureKeyVault
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string secretName = "defaultConnection";
+            var keyVaultName = "anup-strings";
+            var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+            var secret = await client.GetSecretAsync(secretName);
+            Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+        }
+    }
+}
